45595287c4e01760df58fa2f8c7fab890f57abb9
AfafZ <affef2082@gmail.com>
2022-05-11 10:28:49 +0200

    add the third files   challenge

---
+++ ex3.ts
@@ -0,0 +1,28 @@
+class Person {
+
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    tellMyName() {
+      console.log("I am", this.name);
+    }
+  
+    tellMyAge() {
+      console.log("I am", this.age, "years old");
+    }
+  }
+  
+  let john:Person=new Person("John",40)
+  let mary:Person=new Person("Mary",35)
+  
+  john.tellMyName()
+  john.tellMyAge()
+  
+  mary.tellMyName()
+  mary.tellMyAge()
+  
